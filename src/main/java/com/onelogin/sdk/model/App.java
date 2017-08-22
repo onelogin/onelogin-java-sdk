4c071688e83bebbcbc98b2d7e35a6b5de9006c09
Sixto Martin <pitbulk@gmail.com>
2017-08-22 14:08:38 +0200

    Update at App class the provisioned type

--- src/main/java/com/onelogin/sdk/model/App.java
+++ src/main/java/com/onelogin/sdk/model/App.java
@@ -7,7 +7,7 @@ public class App {
 	public long id;
 	public String name;
 	public String icon;
-	public String provisioned;
+	public int provisioned;
 	public Boolean extensionRequired;
 	public int loginId;
 	public Boolean personal;
@@ -16,7 +16,7 @@ public class App {
 		id = data.optLong("id");
 		name = data.optString("name", null);
 		icon = data.optString("icon", null);
-		provisioned = data.optString("provisioned", null);
+		provisioned = data.optInt("provisioned");
 		extensionRequired = data.optBoolean("extension", false);
 		loginId = data.optInt("login_id");
 		personal = data.optBoolean("personal", false);
