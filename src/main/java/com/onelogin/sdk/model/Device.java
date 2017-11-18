c6c8e56dee5d43de418e32c12fe309fbee35800c
Sixto Martin <pitbulk@gmail.com>
2017-11-18 23:19:00 +0100

    Added extra two fields for Duo Security device type

--- src/main/java/com/onelogin/sdk/model/Device.java
+++ src/main/java/com/onelogin/sdk/model/Device.java
@@ -6,15 +6,26 @@ public class Device {
 
 	protected long id;
 	protected String type;
+	protected String duoSigRequest;
+	protected String duoApiHostname;
 
 	public Device(long id, String type) {
 		this.id = id;
 		this.type = type;
 	}
 
+	public Device(long id, String type, String duoSigRequest, String duoApiHostname) {
+		this.id = id;
+		this.type = type;
+		this.duoSigRequest = duoSigRequest;
+		this.duoApiHostname = duoApiHostname;
+	}
+
 	public Device(JSONObject data) {
 		id = data.optLong("device_id");
 		type = data.optString("device_type", null);
+		duoSigRequest = data.optString("duo_sig_request", null);
+		duoApiHostname = data.optString("duo_api_hostname", null);
 	}
 
 	public long getID() {
@@ -23,5 +34,13 @@ public class Device {
 
 	public String getType()	{
 		return type;
-	}	
+	}
+
+	public String getDuoSigRequest() {
+		return duoSigRequest;
+	}
+
+	public String getDuoApiHostname() {
+		return duoApiHostname;
+	}
 }
