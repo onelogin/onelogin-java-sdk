0d0dc4e38a9f7072dbf1b0f860efc2bc76e2eb72
Mohamed Gelbana <mohamed.gelbana@incorta.com>
2018-04-24 19:52:15 +0200

    Resotre original formatting

--- src/main/java/com/onelogin/sdk/model/Group.java
+++ src/main/java/com/onelogin/sdk/model/Group.java
@@ -4,35 +4,35 @@ import org.json.JSONObject;
 
 public class Group {
 
-    protected long id;
-    protected String name;
-    protected String reference;
+	protected long id;
+	protected String name;
+	protected String reference;
 
-    public Group(long id, String name, String reference) {
-        this.id = id;
-        this.name = name;
-        this.reference = reference;
-    }
+	public Group(long id, String name, String reference) {
+		this.id = id;
+		this.name = name;
+		this.reference = reference;
+	}
 
-    public Group(JSONObject data) {
-        id = data.optLong("id");
-        name = data.optString("name", null);
-        reference = data.optString("reference", null);
-    }
+	public Group(JSONObject data) {
+		id = data.optLong("id");
+		name = data.optString("name", null);
+		reference = data.optString("reference", null);
+	}
 
-    public long getID() {
-        return id;
-    }
+	public long getID() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName()	{
+		return name;
+	}
 
-    public String getReference() {
-        return reference;
-    }
-
-    @Override
+	public String getReference() {
+		return reference;
+	}
+	
+	@Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
@@ -49,5 +49,4 @@ public class Group {
         if (id != other.id) return false;
         return true;
     }
-
 }
--- src/main/java/com/onelogin/sdk/model/Role.java
+++ src/main/java/com/onelogin/sdk/model/Role.java
@@ -4,28 +4,28 @@ import org.json.JSONObject;
 
 public class Role {
 
-    protected long id;
-    protected String name;
+	protected long id;
+	protected String name;
 
-    public Role(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public Role(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public Role(JSONObject data) {
-        id = data.optLong("id");
-        name = data.optString("name", null);
-    }
+	public Role(JSONObject data) {
+		id = data.optLong("id");
+		name = data.optString("name", null);
+	}
 
-    public long getID() {
-        return id;
-    }
+	public long getID() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
-
-    @Override
+	public String getName()	{
+		return name;
+	}
+	
+	@Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
