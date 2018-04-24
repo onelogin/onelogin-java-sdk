2764ede4be7ef5fa6f4f06e026afb7e99fd81d36
Mohamed Gelbana <mohamed.gelbana@incorta.com>
2018-04-24 13:25:48 +0200

    Implement `hasCode` and equals methods for User, Group, Role and Event
    `User` and `Event` hashCode uses the `groupId`

--- src/main/java/com/onelogin/sdk/model/Group.java
+++ src/main/java/com/onelogin/sdk/model/Group.java
@@ -4,31 +4,50 @@ import org.json.JSONObject;
 
 public class Group {
 
-	protected long id;
-	protected String name;
-	protected String reference;
+    protected long id;
+    protected String name;
+    protected String reference;
 
-	public Group(long id, String name, String reference) {
-		this.id = id;
-		this.name = name;
-		this.reference = reference;
-	}
+    public Group(long id, String name, String reference) {
+        this.id = id;
+        this.name = name;
+        this.reference = reference;
+    }
 
-	public Group(JSONObject data) {
-		id = data.optLong("id");
-		name = data.optString("name", null);
-		reference = data.optString("reference", null);
-	}
+    public Group(JSONObject data) {
+        id = data.optLong("id");
+        name = data.optString("name", null);
+        reference = data.optString("reference", null);
+    }
 
-	public long getID() {
-		return id;
-	}
+    public long getID() {
+        return id;
+    }
 
-	public String getName()	{
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getReference() {
-		return reference;
-	}	
+    public String getReference() {
+        return reference;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Group other = (Group) obj;
+        if (id != other.id) return false;
+        return true;
+    }
+
 }
--- src/main/java/com/onelogin/sdk/model/Role.java
+++ src/main/java/com/onelogin/sdk/model/Role.java
@@ -4,24 +4,42 @@ import org.json.JSONObject;
 
 public class Role {
 
-	protected long id;
-	protected String name;
+    protected long id;
+    protected String name;
 
-	public Role(long id, String name) {
-		this.id = id;
-		this.name = name;
-	}
+    public Role(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-	public Role(JSONObject data) {
-		id = data.optLong("id");
-		name = data.optString("name", null);
-	}
+    public Role(JSONObject data) {
+        id = data.optLong("id");
+        name = data.optString("name", null);
+    }
 
-	public long getID() {
-		return id;
-	}
+    public long getID() {
+        return id;
+    }
 
-	public String getName()	{
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Role other = (Role) obj;
+        if (id != other.id) return false;
+        return true;
+    }
 }
