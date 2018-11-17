00141a95309511b7c754aa81c33eda42105fc400
Sixto Martin <pitbulk@gmail.com>
2018-11-18 00:56:45 +0100

    Release 1.4.1

--- src/main/java/com/onelogin/sdk/conn/Client.java
+++ src/main/java/com/onelogin/sdk/conn/Client.java
@@ -67,7 +67,7 @@ import com.onelogin.sdk.util.Settings;
 
 public class Client {
 
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.4.1";
 
 	public static final String CUSTOM_USER_AGENT = "onelogin-java-sdk " + VERSION;
 
