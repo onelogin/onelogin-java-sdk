7b560f59fcafa8cd7ac0bbe1a66839c21d5a690f
eriktalvi <erik.talvi@onelogin.com>
2022-05-04 16:21:43 -0700

    Update Client.java

--- src/main/java/com/onelogin/sdk/conn/Client.java
+++ src/main/java/com/onelogin/sdk/conn/Client.java
@@ -88,7 +88,7 @@ import com.onelogin.sdk.util.Settings;
 
 public class Client {
 
-	public static final String VERSION = "2.0.1";
+	public static final String VERSION = "2.0.2";
 
 	public static final String CUSTOM_USER_AGENT = "onelogin-java-sdk " + VERSION;
 
